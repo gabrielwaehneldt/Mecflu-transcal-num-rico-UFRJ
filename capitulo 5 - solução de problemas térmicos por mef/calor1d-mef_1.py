8f2ee51cf30ae16dc935039d44987200559c2517
gabrielwaehneldt <gabrielaffonsocosta@poli.ufrj.br>
2021-10-09 20:42:14 -0300

    2d is partially broken

--- capitulo 5 - solução de problemas térmicos por mef/calor1d-mef_1.py
+++ capitulo 5 - solução de problemas térmicos por mef/calor1d-mef_1.py
@@ -1,12 +1,10 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import mesh as ms
 
 # parametros da simulacao
 L = 1.0
-ne = 4 # numero de elementos da malha quadratica (igual linear)
+ne = 8 # numero de elementos da malha quadratica (igual linear)
 mode_N=3 # Ordem de N
 mode_X=1 # distribuicao dos pontos da malha (linear(1), quadratico(2), gauss("gauss"))
 Te = 100.0
@@ -34,10 +32,10 @@ if mode_N!=1:
 IEN=np.array(IEN)  #transformando lista em array 
 
 # plot da condicao de contorno de T
-T = np.zeros( (nx),dtype='float' )
-T[0]=Te
-T[-1]=Td #condições de temperatura na ponta
-plt.plot(X,T,'r-') #plota condição de temperatura inicial
+#T = np.zeros( (nx),dtype='float' )
+#T[0]=Te
+#T[-1]=Td #condições de temperatura na ponta
+#plt.plot(X,T,'r-') #plota condição de temperatura inicial
 
 # inicializacao das matrizes e vetores
 K = np.zeros( (nx,nx),dtype='float' )
@@ -45,36 +43,40 @@ M = np.zeros( (nx,nx),dtype='float' )
 f = np.zeros( (nx),dtype='float' )
 # loop nos elementos
 #-------------------------------------------------- 
-for e in range(0,ne):
- if mode_N==1: #N linear
+
+if mode_N==1: #N linear
+ for e in range(0,ne):
   [v1,v2] = IEN[e] 
   # calculo do comprimento do elemento
   h = X[v2]-X[v1]
   # matrizes e vetores do elemento mestre
   kelem = (1.0/h)*np.array([ [1,-1],
-                             [-1,1] ])
+                              [-1,1] ])
   felem = (h/2.0)*np.array( [1, 1] )
- if mode_N==2: # N quadrado
-  [v1,v2,v3] = IEN[e] 
-  # calculo do comprimento do elemento quadratico
-  h = X[v3]-X[v1]
-  # matrizes e vetores do elemento mestre
-  kelem = (1.0/(3*h))*np.array([ [7,-8,1],
+if mode_N==2: # N quadrado
+  for e in range(0,ne):
+   [v1,v2,v3] = IEN[e] 
+   # calculo do comprimento do elemento quadratico
+   h = X[v3]-X[v1]
+   # matrizes e vetores do elemento mestre
+   kelem = (1.0/(3*h))*np.array([ [7,-8,1],
                                  [-8,16,-8], 
                                  [1,-8,7] ]) 
   felem = (h/6.0)*np.array( [1,4,1] )
- if mode_N==3: # N cubico
-  [v1,v2,v3,v4] = IEN[e] 
-  # calculo do comprimento do elemento quadratico
-  h = X[v4]-X[v1]
-  # matrizes e vetores do elemento mestre
-  kelem = (1.0/(40*h))*np.array([ [148,-189,54,-13],
-                                  [-189,432,-297,54],
-                                  [54,-297,432,-189],
-                                  [-13,54,-189,148]]) 
-  felem = (h/8.0)*np.array( [1,3,3,1] )
+if mode_N==3: # N cubico
+  for e in range(0,ne):
+   [v1,v2,v3,v4] = IEN[e] 
+   # calculo do comprimento do elemento quadratico
+   h = X[v4]-X[v1]
+   # matrizes e vetores do elemento mestre
+   kelem = (1.0/(40*h))*np.array([ [148,-189,54,-13],
+                                   [-189,432,-297,54],
+                                   [54,-297,432,-189],
+                                   [-13,54,-189,148]]) 
+   felem = (h/8.0)*np.array( [1,3,3,1] )
 
  # loops nos pontos do elemento e
+for e in range(0,ne):
  for i in range(0,mode_N+1):
   iglobal = IEN[e,i]
   f[iglobal] += q/(rho*cv)*felem[i]
@@ -107,4 +109,3 @@ plt.xlabel('comprimento da barra [m]')
 plt.ylabel('temperatura [oC]')
 plt.title("Calor 1d mef com geração de calor") # adicionando titulo ao plot
 plt.show()
-
